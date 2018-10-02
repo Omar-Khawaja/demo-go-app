cca7f441821debce3570d40a512e47d95ca4be4b
Omar Khawaja <khawaja910@gmail.com>
2018-10-02 18:38:39 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+var version = 3
+var color = "green"
+var node = os.Getenv("NODE_IP")
+var intro = fmt.Sprintf(
+	"<!DOCTYPE html>"+
+		"<html>"+
+		"<body>"+
+
+		"<h1 style=\"color:%s;\">Welcome! This is <i>version %d</i> of your application!</h1>"+
+		"<h1 style=\"color:%s;\">You are on node %v"+
+
+		"</body>"+
+		"</html>", color, version, color, node)
+
+func giveIntro(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, intro)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    fmt.Sprintf("0.0.0.0:%v", port),
+		Handler: mux,
+	}
+	mux.HandleFunc("/", giveIntro)
+	server.ListenAndServe()
+}
